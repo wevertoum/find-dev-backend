bde66682816f911b1735ce2c35f3cf2561741975
Weverton Rodrigues <wevertonmixalt@gmail.com>
2020-01-15 12:52:56 -0300

    projeto backend, semana omnistack private mode

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# find-dev-backend
+# omnistack-backend
+Projeto feito com o intuito de estudar desenvolvimento backend NodeJS em ambiente React/MongoDB
+
+Para baixar dependencias e executar aplicação  
+```yarn init -y```  
+```yarn dev```
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes')
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+mongoose.connect(`mongodb+srv://${process.env.API_USER}:${process.env.API_KEY}@cluster0-a26sr.mongodb.net/week10?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(process.env.PORT);
